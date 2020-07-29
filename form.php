4f79f04f182fb07c53ef3c60f8154b9e16ef336b
killnumber <58339396+killnumber254@users.noreply.github.com>
2020-07-29 11:33:13 +0300

    New
    New

--- form.php
+++ form.php
@@ -1,3 +1,11 @@
 <?php
-echo "Hello World";
-?>
+ 
+ if($_POST['nikNameValue']){
+   $nik = $_POST['nikNameValue'];
+ }
+ 
+ $text = $_POST['text'];
+ 
+ mail("$nik", '$text');
+ echo '<p>Сообщение отправлено</p>'
+ ?>
--- script.js
+++ script.js
@@ -1,75 +1,73 @@
 const generateId = () =>
-  `gloId ${Math.random(Math.random() + 1e8).toString(16)}`;
-const wind = document.querySelector(".window");
-const texter = document.getElementById("texter");
-const btn = document.getElementById("btn");
+  `gloId ${Math.random(1e8).toString(16)}`
+const wind = document.querySelector('.window');
+const texter = document.getElementById('texter');
+const btn = document.getElementById('btn');
 const modal = document.getElementById("myModal");
-const nikName = document.getElementById("nikname");
-const btnOne = document.getElementById("btnOne");
+const nikName = document.getElementById("nikname")
+const btnOne = document.getElementById("btnOne")
+
+
+
 
 let Document = JSON.parse(localStorage.getItem("calc")) || [];
 
 let Documentos = JSON.parse(localStorage.getItem("calcs")) || [];
 
 const operationText = (obj) => {
-  const listItem = document.createElement("li");
-  listItem.innerHTML = `
-${obj.text}`;
-  wind.prepend(listItem);
-};
-const optionText = (event) => {
-  event.preventDefault();
-  console.log(event);
-  const optionValue = texter.value;
+  
+const listItem = document.createElement('li')
+listItem.innerHTML = `
+${obj.text}`
+wind.prepend(listItem);
+}
 
-  if (optionValue) {
+const optionText = (event) =>{
+  console.log(event)
+  const optionValue = texter.value;
+  if(optionValue) {
     const obj = {
-      //id: generateId,
-      // name: 'serg',
       text: optionValue,
-    };
+    }
+   
     Document.push(obj);
+    optionModal();
     init();
   }
-  optionModal();
-  texter.value = "";
-};
-const optionModal = (event) => {
-  const nikNameValue = nikName.value;
+  texter.value = '';
+}
 
-  if (nikNameValue) {
-    const objOne = {
-      id: generateId(),
+const optionModal = (event) => {
+ let nikNameValue = nikName.value;
+
+if(nikNameValue) {
+     const objOne = {
       name: nikNameValue,
-    };
-    //let response = fetch("/article/fetch/post/objOne", {
-    //  method: "POST",
-    //  headers: {
-    //    "Content-Type": "application/json;charset=utf-8",
-    //  },
-    //  body: JSON.stringify(objOne),
-    //});
-    //
-    //let result = response.json;
-    //
-    console.log(objOne.id);
-    console.log(objOne.name);
+    }
+    console.log( objOne.name);
     Documentos.push(objOne);
     init();
   }
-  modal.style.display = "none";
-};
-const init = () => {
-  wind.textContent = "";
+ modal.style.display = "none";
+}
+
+const init = () =>{
+  wind.textContent = '';
   Document.forEach(operationText);
   localStorage.setItem("calc", JSON.stringify(Document));
-  localStorage.setItem("calcs", JSON.stringify(Documentos));
-};
+    localStorage.setItem("calcs",JSON.stringify(Documentos));
+}
+
 const delet = () => {
   localStorage.clear("calc");
-};
+  localStorage.clear("calcs");
+}
+
+init();
+
 setInterval(delet, 700);
 
-btn.addEventListener("click", optionText);
-btnOne.addEventListener("click", optionModal);
-init();
+btn.addEventListener(("click"), optionText);      
+
+btnOne.addEventListener(('click'), 
+optionModal);
